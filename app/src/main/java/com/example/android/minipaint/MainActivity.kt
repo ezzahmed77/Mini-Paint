37d910319c7ff227b173013c60f5c3b4c0fdadef
ezzahmed77 <ezzahmed.wor@gmail.com>
2023-04-17 16:35:55 +0200

    Solved an error in inflating the CanvasView

--- app/src/main/java/com/example/android/minipaint/MainActivity.kt
+++ app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.android.minipaint
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var canvasView: CanvasView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        val canvasView = CanvasView(this)
+        canvasView = CanvasView(context = this)
+        setContentView(canvasView)
 
-        setContentView(R.layout.activity_main)
 
     }
 }
